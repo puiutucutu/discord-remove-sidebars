f7c3f1e92eae555968bacd4967cb2bb448563150
puiu <5474275+puiutucutu@users.noreply.github.com>
2021-05-10 22:28:08 -0400

    Create discord - remove sidebar.user.js

---
+++ discord - remove sidebar.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         discord - remove sidebar
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://discord.com/channels/*/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  window.addEventListener("load", function () {
+    const navs = document.getElementsByTagName("nav");
+    for (let nav of navs) {
+      nav.style.display = "none";
+    }
+
+    const re = /sidebar-[^ ]*/gm;
+    const divs = document.getElementsByTagName("div");
+    for (let div of divs) {
+      const className = div.className;
+      console.log(className);
+
+      if (!!re.test(className)) {
+        div.style.display = "none";
+      }
+    }
+  });
+})();
